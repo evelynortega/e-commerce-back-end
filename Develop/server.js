ab024c2564b307df352bc0f8362da6c7f1f749fb
saanspeur <evelin.ortega018@gmail.com>
2023-07-03 23:55:39 -0700

    finished seed files

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
+// added "const sequelize"
+const sequelize = require('./config/connection')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+// added serquelize.sync
+sequelize.sync({force: false}).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+});
+
+// cloned code
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
